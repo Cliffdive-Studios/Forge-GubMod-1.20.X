9600f7f5afe19f917c71c96ec44051a0cdf61bd4
Char <akmeduri1@gmail.com>
2024-05-27 17:55:01 -0700

    Removed Test Line

--- src/main/java/net/chargon/gubmod/GubMod.java
+++ src/main/java/net/chargon/gubmod/GubMod.java
@@ -20,7 +20,6 @@ public class GubMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "gubmod";
     // Directly reference a slf4j logger
-    //Test Line
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public GubMod() {
