4464c1c8e2fed9e2ed67dd836ace2fafb7c4d7fd
Char <akmeduri1@gmail.com>
2024-05-27 17:32:51 -0700

    Test Push

--- src/main/java/net/chargon/gubmod/GubMod.java
+++ src/main/java/net/chargon/gubmod/GubMod.java
@@ -20,7 +20,7 @@ public class GubMod
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "gubmod";
     // Directly reference a slf4j logger
-    // Test
+    // Test 2
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public GubMod() {
